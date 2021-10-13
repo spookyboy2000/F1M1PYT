7be27b83b431976ab2f02d6d21a180cdf9920c9e
Nick <32973@ma-web.nl>
2021-10-13 11:29:16 +0200

    hjfbaw

---
+++ string.py
@@ -0,0 +1,116 @@
+
+s = "Hallo ik ben een string en ik wordt opgegeten"
+
+ss = s[0:len(s)-1]
+print(ss)
+
+ss = s[0:len(s)-2]
+print(ss)
+
+ss = s[0:len(s)-3]
+print(ss)
+
+ss = s[0:len(s)-4]
+print(ss)
+
+ss = s[0:len(s)-5]
+print(ss)
+
+ss = s[0:len(s)-6]
+print(ss)
+
+ss = s[0:len(s)-7]
+print(ss)
+
+ss = s[0:len(s)-8]
+print(ss)
+
+ss = s[0:len(s)-10]
+print(ss)
+
+ss = s[0:len(s)-11]
+print(ss)
+
+ss = s[0:len(s)-12]
+print(ss)
+
+ss = s[0:len(s)-13]
+print(ss)
+
+ss = s[0:len(s)-14]
+print(ss)
+
+ss = s[0:len(s)-16]
+print(ss)
+
+ss = s[0:len(s)-17]
+print(ss)
+
+ss = s[0:len(s)-19]
+print(ss)
+
+ss = s[0:len(s)-20]
+print(ss)
+
+ss = s[0:len(s)-21]
+print(ss)
+
+ss = s[0:len(s)-23]
+print(ss)
+
+ss = s[0:len(s)-24]
+print(ss)
+
+ss = s[0:len(s)-25]
+print(ss)
+
+ss = s[0:len(s)-26]
+print(ss)
+
+ss = s[0:len(s)-27]
+print(ss)
+
+ss = s[0:len(s)-28]
+print(ss)
+
+ss = s[0:len(s)-30]
+print(ss)
+
+ss = s[0:len(s)-31]
+print(ss)
+
+ss = s[0:len(s)-32]
+print(ss)
+
+ss = s[0:len(s)-34]
+print(ss)
+
+ss = s[0:len(s)-35]
+print(ss)
+
+ss = s[0:len(s)-36]
+print(ss)
+
+ss = s[0:len(s)-38]
+print(ss)
+
+ss = s[0:len(s)-39]
+print(ss)
+
+ss = s[0:len(s)-41]
+print(ss)
+
+ss = s[0:len(s)-42]
+print(ss)
+
+ss = s[0:len(s)-43]
+print(ss)
+
+ss = s[0:len(s)-44]
+print(ss)
+
+ss = s[0:len(s)-45]
+print(ss)
+
+ss = s[0:len(s)-0]
+print(ss)
