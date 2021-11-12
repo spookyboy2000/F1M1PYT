8ad441600675fdaf0d2ec495fafc3a0210bf722b
spookyboy2000 <54852313+spookyboy2000@users.noreply.github.com>
2021-11-12 09:18:12 +0100

    Delete stutter.py

--- stutter.py
+++
@@ -1,9 +0,0 @@
-running = True
-while running == True:
-    antwoord = input("Type hier een zin:  ")
-    user = antwoord.split()
-    for x in user:
-        if len(x) >3:
-            print(x[0:2]+"...", x[0:2]+"..." ,x)
-        else:
-            print(x)
