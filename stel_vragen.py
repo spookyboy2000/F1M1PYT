3d45f2ce594f96ffbd23ccb2cdb0071777b7f086
spookyboy2000 <54852313+spookyboy2000@users.noreply.github.com>
2021-11-12 09:18:25 +0100

    Delete stel_vragen.py

--- stel_vragen.py
+++
@@ -1,8 +0,0 @@
-antwoord = input("Hoe oud ben je? ")
-print("Aha, je bent dus " + antwoord + " jaar oud, leuk zeg!")
-
-antwoord = input("waar kom je vandaan? ")
-print("Aha, je komt dus uit " + antwoord + " leuk zeg!")
-
-antwoord = input("wat is je naam? ")
-print("Aha, je bent dus " + antwoord + " leuk zeg!")
