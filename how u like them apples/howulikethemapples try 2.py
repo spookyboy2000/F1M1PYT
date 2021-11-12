ed6b15a2c7cbe49d53b8efeaa22228e2fd224ca5
spookyboy2000 <54852313+spookyboy2000@users.noreply.github.com>
2021-11-12 09:47:23 +0100

    Add files via upload

---
+++ how u like them apples/howulikethemapples try 2.py
@@ -0,0 +1,17 @@
+a = int(input("Amount appelbomen: "))
+b = int(input("hoeveel appels krijg je in de zon: "))
+c = int(input("hoeveel appels krijg je in de schaduw: "))
+d = int(input("aantal bomen in schaduw: "))
+e = int(input("aantal bomen in zon: "))
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+g = (e * b + d * c)
+
+print(g)
+f = g/4
+print(f)
